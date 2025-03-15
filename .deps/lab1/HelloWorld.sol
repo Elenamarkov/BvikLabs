ef194c20f711d35c3341b7dbebdc5d24017a2ecd
Elenamarkov <elena.elenamarkovska@gmail.com>
2025-03-15 11:54:36 +0100

    update new

---
+++ .deps/lab1/HelloWorld.sol
@@ -0,0 +1,18 @@
+//SPDX-Licence-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    string private value;  
+
+    function store(string memory newValue) public { 
+        value = newValue;
+    }
+    
+    function retrieve() public view returns (string memory) {
+      return value;
+  }
+
+}
+
+
